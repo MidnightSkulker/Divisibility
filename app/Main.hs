0bb5731cb64c66e09276c45decd6a06d6815f9fb
Peter White <desourdesourde@gmail.com>
2020-10-19 11:11:21 -0700

    Spacing

--- app/Main.hs
+++ app/Main.hs
@@ -5,17 +5,20 @@ import Data.List (unfoldr)
 
 main :: IO ()
 main = do
-  putStrLn $ "Hello World: " ++ show (digits 7463)
   putStrLn $ "93457983475984 is divisible by 2: " ++ show (divisibleByTwo 93457983475984)
   putStrLn $ "93457983475985 is divisible by 2: " ++ show (divisibleByTwo 93457983475985)
+
   putStrLn $ "Sums of digits of 387483402019012987654321: " ++ show (sumsOfDigits 30 387483402019012987654321)
   putStrLn $ "Sums of digits of 3636363636363636: " ++ show (sumsOfDigits 30 3636363636363636)
+
   putStrLn $ "387483402019012987654321 is divisible by 3: " ++ show (divisibleByThree 387483402019012987654321)
   putStrLn $ "3636363636363636 is divisible by 3: " ++ show (divisibleByThree 3636363636363636)
+
   putStrLn $ "387483402019012987654321 is divisible by 4: " ++ show (divisibleByFour 387483402019012987654321)
   putStrLn $ "3636363636363636 is divisible by 4: " ++ show (divisibleByFour 3636363636363636)
-  putStrLn $ "387483402019012987654321 is divisible by 4: " ++ show (divisibleByFive 387483402019012987654321)
-  putStrLn $ "3636363636363635 is divisible by 4: " ++ show (divisibleByFive 3636363636363635)
+  putStrLn $ "387483402019012987654321 is divisible by 5: " ++ show (divisibleByFive 387483402019012987654321)
+  putStrLn $ "3636363636363635 is divisible by 5: " ++ show (divisibleByFive 3636363636363635)
+
   putStrLn $ "387483402019012987654321 is divisible by 6: " ++ show (divisibleBySix 387483402019012987654321)
   putStrLn $ "3636363636363636 is divisible by 6: " ++ show (divisibleBySix 3636363636363636)
 
