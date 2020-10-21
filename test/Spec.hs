b6560af27c01144b9ead070054c046b7311a1cac
Peter White <desourdesourde@gmail.com>
2020-10-21 10:48:23 -0700

    Framework for using HUnit set up

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,10 @@
+module Test where
+
+import Test.HUnit
+-- import Main(sumsOfDigits)
+
 main :: IO ()
 main = putStrLn "Test suite not yet implemented"
+
+testFoo :: Test
+testFoo = TestCase $ assertEqual "Should return 2" 5 2
