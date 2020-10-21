2c8ba339793420bf6d575158b63220f8c97b07e0
Peter White <desourdesourde@gmail.com>
2020-10-21 13:56:49 -0700

    Moved divisible by 4 and 5 to HUnit Spec

--- test/Spec.hs
+++ test/Spec.hs
@@ -18,9 +18,21 @@ testDiv3_1 :: Test
 testDiv3_1 = TestCase $ assertEqual "divisibleByThree.1" (divisibleByThree 387483402019012987654321) False
 testDiv3_2 :: Test
 testDiv3_2 = TestCase $ assertEqual "divisibleByThree.2" (divisibleByThree 3636363636363636) True
+testDiv4_1 :: Test
+testDiv4_1 = TestCase $ assertEqual "divisibleByFour.1"  (divisibleByFour 387483402019012987654321) False
+testDiv4_2 :: Test
+testDiv4_2 = TestCase $ assertEqual "divisibleByFour.2" (divisibleByFour 3636363636363636) True
+testDiv5_1 :: Test
+testDiv5_1 = TestCase $ assertEqual "divisibleByFive.1" (divisibleByFive 387483402019012987654321) False
+testDiv5_2 :: Test
+testDiv5_2 = TestCase $ assertEqual "divisibleByFive.1" (divisibleByFive 3636363636363635) True
 
 tests :: Test = TestList [ TestLabel "divisible by 2 - #1" testDiv2_1
                          , TestLabel "divisible by 2 - #2" testDiv2_2
                          , TestLabel "divisible by 3 - #1" testDiv3_1
                          , TestLabel "divisible by 3 - #2" testDiv3_2
+                         , TestLabel "divisible by 4 - #1" testDiv4_1
+                         , TestLabel "divisible by 4 - #2" testDiv4_2
+                         , TestLabel "divisible by 5 - #1" testDiv5_1
+                         , TestLabel "divisible by 5 - #2" testDiv5_2
                          ]
